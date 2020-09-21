009e05aa8fd3b8a41540b1f07e5ffe8ae51297cb
Danny <dannyconcepcion01@gmail.com>
2020-09-20 20:17:02 -0400

    Agregar archivos de proyecto.

---
+++ programa_3_practica_4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_3_practica_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aritmetica operaciones = new Aritmetica();
+            operaciones.Suma();
+            operaciones.resta();
+            operaciones.multi();
+            operaciones.division();
+
+
+            
+            
+
+           
+
+            
+
+
+
+        }
+    }
+}
